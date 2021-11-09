ad45a9e61d567aa88443e77f1027ed4a85be371c
danilson <danielE>
2021-11-09 10:55:32 -0500

    run using mvn

--- src/test/java/com/Recordatorio/Entrevista/Runners/Runner.java
+++ src/test/java/com/Recordatorio/Entrevista/Runners/Runner.java
@@ -7,7 +7,7 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = { "src/test/resources/features/" }, glue = { "com.Recordatorio.Entrevista.Utilities",
-		"com.Recordatorio.Entrevista.StepsDefinition" }, tags = "@ValidationOne")
+		"com.Recordatorio.Entrevista.StepsDefinition" }, tags = "@ValidationOne", publish = true)
 
 public class Runner {
 
