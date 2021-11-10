1c0a003f222b12a836b7d3bc5a61e54343a3c21e
danilson <danielE>
2021-11-09 22:15:49 -0500

    cambio reporte

--- src/test/java/com/Recordatorio/Entrevista/Runners/Runner.java
+++ src/test/java/com/Recordatorio/Entrevista/Runners/Runner.java
@@ -7,7 +7,9 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = { "src/test/resources/features/" }, glue = { "com.Recordatorio.Entrevista.Utilities",
-		"com.Recordatorio.Entrevista.StepsDefinition" }, tags = "@ValidationOne", publish = true)
+		"com.Recordatorio.Entrevista.StepsDefinition" }, tags = "@ValidationOne", publish = true, plugin = { "pretty",
+				"json:target/cucumber-reports/Cucumber.json", "junit:target/cucumber-reports/Cucumber.xml",
+				"html:target/cucumber-reports" }, monochrome = true)
 
 public class Runner {
 
